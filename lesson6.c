d2a95fcad9baa867d1a1b94319e9bf956e01a938
Lauren <laurenpehlivanian@gmail.com>
2019-09-19 16:07:05 +0100

    copied code from school computer using ssh

---
+++ lesson6.c
@@ -0,0 +1,36 @@
+int main() {
+  char c = 1;
+  int i = 238;
+  long l = 1356;
+
+  //The addresses are subsequent in hex
+  printf("%p %p %p\n", &c, &i, &l);
+
+  //create pointer objectds:
+   char *ch; //ch = char pointer
+   int *ip;
+   long *lp;
+
+   ch = &c;
+   ip = &i;
+   lp = &l;
+
+   //modify orig vars with pointer objectds
+   c = *ch + 1;
+   i = *ip - 39;
+   l = *lp * .5;
+
+   printf("%c\n",c);
+   printf("%c %i %ld\n", c, i, l);
+
+   //create char type pointer
+   char *toast;
+   toast = &i;
+
+   printf("%p: %i\n", toast, *toast);
+
+   //modify value using pointer
+   i = *toast - 9;
+
+   printf("%i\n", i);
+}
